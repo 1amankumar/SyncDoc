cc3dad24273ff058d09d30bb090d17b5c154a1fc
1amankuma <aman4578kumar@gmail.com>
2026-09-17 23:08:22 +0530

    CRDT/WebSocket synchronization foundation for SyncDoc

--- backend/src/websocket/websocketServer.ts
+++ backend/src/websocket/websocketServer.ts
@@ -1,12 +1,38 @@
 import { WebSocketServer } from "ws";
+import * as Y from "yjs";
 
-const ws=new WebSocketServer({port:5001})
+const ydoc = new Y.Doc();
 
-ws.on("connection",(socket)=>{
+const document = ydoc.getMap("document");
+
+const ws = new WebSocketServer({ port: 5001 })
+ydoc.on("update", (update) => {
+    console.log("yjs document updated");
+    ws.clients.forEach((client) => {
+        if (client.readyState == 1) {
+            client.send(update);
+        }
+    })
+})
+document.set("title", "My first Syncdoc");
+
+const update = Y.encodeStateAsUpdate(ydoc);
+console.log("yjs update-", update)
+
+ws.on("connection", (socket) => {
     console.log("client connected successfully")
 
-    socket.on("message",(message)=>{
-        console.log("Message Received -",message.toString());
-        socket.send("hello from web socket server");
+    socket.on("message", (message) => {
+        console.log("Message Received -", message.toString());
+        const update = new Uint8Array(message as Buffer);
+
+        Y.applyUpdate(ydoc, update);
+        ws.clients.forEach((client) => {
+            if (client !== socket && client.readyState === 1) {
+                client.send(update);
+            }
+        });
     })
+
+
 })
--- frontend/src/services/websocketService.ts
+++ frontend/src/services/websocketService.ts
@@ -1,9 +1,19 @@
-const socket=new WebSocket("ws://localhost:5001");
+import * as Y from "yjs";
 
-socket.onopen=()=>{
+const ydoc = new Y.Doc();
+const document = ydoc.getMap("document");
+
+document.set("title", "Hello from React");
+
+const update = Y.encodeStateAsUpdate(ydoc);
+
+
+const socket = new WebSocket("ws://localhost:5001");
+
+socket.onopen = () => {
     console.log("Connected to WebSocket server");
+    socket.send(update)
 
-    socket.send("Hello from React");
 }
 socket.onmessage = (event) => {
     console.log("Message from server:", event.data);
